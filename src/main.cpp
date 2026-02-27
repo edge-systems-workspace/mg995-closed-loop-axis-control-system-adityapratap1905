5b10f8cf10cfe2a84e597d73666d3d5ed88ac341
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:50:32 +0530

    add setup function for initialization in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,13 @@ int trigPin=9;
 int echoPin=10;
 
 
+void setup() {
+    // write your initialization code heregit oush
+    pinMode(trigPin,OUTPUT);
+    pinMode(echoPin,INPUT);
+    Serial.begin(9600);
+    myServo.attach(300);
+
+}
+
+
