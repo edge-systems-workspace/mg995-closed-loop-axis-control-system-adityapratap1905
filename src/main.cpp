133c3939fd8574b52e0fdba96e33fb2d44013690
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:50:49 +0530

    add loop function for distance measurement and servo control in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,28 @@ void setup() {
 }
 
 
+void loop() {
+    // write your code here
+    digitalWrite(trigPin,0); // 0 is for LOW and for OFF
+    delayMicroseconds(2);
+    digitalWrite(trigPin,1); // 1 is for HIGH and for ON
+    delayMicroseconds(10);
+    digitalWrite(trigPin,0); //  0 is for LOW and for OFF
+
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.0343 /2;
+    if(distance<=15)
+    {
+        myServo.write(90);
+    }
+    else
+    {
+        myServo.write(0);
+    }
+    Serial.print("Distance:");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    delay(500);
+
+}
