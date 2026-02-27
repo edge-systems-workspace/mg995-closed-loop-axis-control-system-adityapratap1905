e6b660c126e744b51fe763c7ed92e3bcd2877840
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:50:15 +0530

    remove setup and loop functions from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,3 @@ int trigPin=9;
 int echoPin=10;
 
 
-void setup() {
-    // write your initialization code heregit oush
-    pinMode(trigPin,OUTPUT);
-    pinMode(echoPin,INPUT);
-    Serial.begin(9600);
-    myServo.attach(300);
-
-}
-
-
-void loop() {
-    // write your code here
-    digitalWrite(trigPin,0); // 0 is for LOW and for OFF
-    delayMicroseconds(2);
-    digitalWrite(trigPin,1); // 1 is for HIGH and for ON
-    delayMicroseconds(10);
-    digitalWrite(trigPin,0); //  0 is for LOW and for OFF
-
-    long duration = pulseIn(echoPin, HIGH);
-    long distance = duration * 0.0343 /2;
-    if(distance<=15)
-    {
-        myServo.write(90);
-    }
-    else
-    {
-        myServo.write(0);
-    }
-    Serial.print("Distance:");
-    Serial.print(distance);
-    Serial.println(" cm");
-
-    delay(500);
-
-}
